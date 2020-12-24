ef2a10db3c0d46b7fe8d6b743048a40222ae8e92
SonaliBhagwatkar <sonali.bhagwatkar@gmail.com>
2020-12-24 12:24:25 +0530

    genres filter added

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -10,6 +10,13 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import FormControl from '@material-ui/core/FormControl';
 import Typography from '@material-ui/core/Typography';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import genres from '../../common/genres';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
 
 const styles = (theme) => ({
   root: {
@@ -29,19 +36,36 @@ const styles = (theme) => ({
   },
   gridListMain: {
     transform: 'translateZ(0)',
-    cursor: 'pointer'
-},
-formControl: {
+    cursor: 'pointer',
+  },
+  formControl: {
     margin: theme.spacing.unit,
     minWidth: 240,
-    maxWidth: 240
- },
- title: {
+    maxWidth: 240,
+  },
+  title: {
     color: theme.palette.primary.light,
- }	    
+  },
 });
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movieName: '',
+      genres: [],
+    };
+  }
+
+  movieNameChangeHandler = (event) => {
+    this.setState({ movieName: event.target.value });
+    console.log(this.state.movieName);
+  };
+
+  genresSelectHandler = event => {
+    this.setState({genres: event.target.value});
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -94,16 +118,42 @@ class Home extends Component {
             </GridList>
           </div>
           <div className='right'>
-          <Card>
-          <CardContent>          
-          <FormControl className={classes.formControl}>
-          <Typography className={classes.title} color="textSecondary">
-             FIND MOVIES BY:
-          </Typography>
-          </FormControl>
-          </CardContent>
-          </Card>
-        </div>
+            <Card>
+              <CardContent>
+                <FormControl className={classes.formControl}>
+                  <Typography className={classes.title} color='textSecondary'>
+                    FIND MOVIES BY:
+                  </Typography>
+                </FormControl>
+                <FormControl className={classes.formControl}>
+                  <InputLabel htmlFor='movieName'>Movie Name: </InputLabel>
+                  <Input id='movieName' onChange={this.movieNameChangeHandler} />                   
+                </FormControl>
+                <FormControl className={classes.formControl}>
+                  <InputLabel htmlFor='selectMultipleCheckbox'>
+                    Genres
+                  </InputLabel>
+                  <Select
+                    multiple
+                    input={<Input id='select-multiple-checkBox' />}
+                    renderValue={(selected) => selected.join(',')}
+                    value={this.state.genres}
+                    onChange={this.genresSelectHandler}
+                  >
+                    <MenuItem value='0'>None</MenuItem>
+                    {genres.map((genre) => (
+                      <MenuItem key={genre.id}  >
+                        <Checkbox
+                          checked={this.state.genres.indexOf(genre.name) > -1}
+                        />
+                        <ListItemText primary={genre.name} />
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </CardContent>
+            </Card>
+          </div>
         </div>
       </div>
     );
